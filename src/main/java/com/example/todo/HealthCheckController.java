1c27c39caed78a70c232fcc8645bc9897f15ae46
taecode <gitty58@gmail.com>
2023-01-18 14:18:35 +0900

    #초기 설정 완료

---
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.example.todo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HealthCheckController { //서버 잘 돌아가는지 확인용도 localhost:8080
+    @GetMapping("/")
+    public ResponseEntity<?> check(){
+        log.info("server is running...");
+        return ResponseEntity
+                .ok()
+                .body("server is running...");
+    }
+}
