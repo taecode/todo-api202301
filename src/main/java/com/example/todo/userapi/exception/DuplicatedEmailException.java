946d07f36de3b7eeb0c7185a126e2dd970c02877
taecode <gitty58@gmail.com>
2023-01-26 11:14:20 +0900

    # user api- 회원 가입 처리 기능, 예외처리

---
+++ src/main/java/com/example/todo/userapi/exception/DuplicatedEmailException.java
@@ -0,0 +1,12 @@
+package com.example.todo.userapi.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DuplicatedEmailException extends RuntimeException {
+
+    public DuplicatedEmailException(String message){
+        super(message);
+    }
+
+}
