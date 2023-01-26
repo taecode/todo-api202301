1b5881c070f48c42a9b601c9120905cf83f6a31f
taecode <gitty58@gmail.com>
2023-01-26 10:58:32 +0900

    # user service -create 패스워드 인코딩 처리

---
+++ src/test/java/com/example/todo/userapi/service/UserServiceTest.java
@@ -0,0 +1,55 @@
+package com.example.todo.userapi.service;
+
+import com.example.todo.userapi.dto.UserSignUpDTO;
+import com.example.todo.userapi.dto.UserSignUpResponseDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    @DisplayName("중복된 이메일이 포함된 회원정보로 가입하면 RuntimeException 이 발생해야 한다.")
+    void validateEmailTest(){
+        //given
+        UserSignUpDTO dto = UserSignUpDTO.builder()
+                .email("abc1234@def.com")
+                .password("1234")
+                .userName("키키킥")
+                .build();
+
+        //when
+        userService.create(dto);
+
+        //them
+        assertThrows(RuntimeException.class,()->{
+           userService.create(dto);
+        });
+    }
+
+
+    @Test
+    @DisplayName("검증된 회원정보로 가입하면 회원가입에 성공해야 한다.")
+    void createTest(){
+        //given
+        UserSignUpDTO dto = UserSignUpDTO.builder()
+                .email("zzzz922878@def.com")
+                .password("1234")
+                .userName("암호맨")
+                .build();
+
+        //when
+        UserSignUpResponseDTO responseDTO = userService.create(dto);
+
+        //then
+        System.out.println("responseDTO = " + responseDTO);
+        assertEquals("암호맨",responseDTO.getUserName());
+    }
+}
