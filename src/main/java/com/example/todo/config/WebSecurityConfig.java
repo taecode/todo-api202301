6d0545a0847b1d0b6988b70454354ec1aab3b599
taecode <gitty58@gmail.com>
2023-01-27 14:53:31 +0900

    # 토큰 검사 기능 및 토큰 필터 생성

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,37 +1,53 @@
 package com.example.todo.config;
 
+import com.example.todo.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    //패스워드 인코딩 클래스를 등록
-    //<bean id=? class=? / >
+    private final JwtAuthFilter jwtAuthFilter;
+
+    // 패스워드 인코딩 클래스를 등록
+    // <bean id=? class=? />
     @Bean
-    public BCryptPasswordEncoder encoder(){
+    public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
-    //시큐리티 설정
+    // 시큐리티 설정
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-        //시큐리티 빌더
-        http.cors() //크로스 오리진 정책
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 시큐리티 빌더
+        http.cors() // 크로스오리진정책
                 .and()
-                .csrf()//CSRF정책
-                .disable()//사용 안함() 시큐리티에서 기본 설정 사용 안할거다
-                .httpBasic().disable() //기본 시큐리티 인증 해체, 토큰 인증 쓸꺼기 때문에
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 기반 인증 안함
+                .csrf() // CSRF정책
+                .disable() // 사용 안함
+                .httpBasic().disable() // 기본 시큐리티 인증 해제, 토큰인증 쓸꺼니까
+                // 세션 기반 인증 안함
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().antMatchers("/","/api/auth/**").permitAll() //인증 요청중에서 '/' 경로랑 '/api/auth' 로 시작되는 경로는 인증하지 않고 모두 허용
-                .anyRequest().authenticated(); //그 외의 모든 경로는 인증을 거쳐야함.
+                // 인증 요청중에서 '/'경로랑 '/api/auth'로 시작되는 경로는 인증하지 않고 모두 허용
+                .authorizeRequests().antMatchers("/", "/api/auth/**").permitAll()
+                // 그 외의 모든 경로는 인증을 거쳐야함.
+                .anyRequest().authenticated();
+
+        // 토큰 인증 필터 등록
+        http.addFilterAfter(
+                jwtAuthFilter,
+                CorsFilter.class // import 주의 : 스프링껄로
+        );
+
         return http.build();
     }
+
 }
