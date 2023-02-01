944c0a676b4f94880c7ab282812c4427d6b25aa3
taecode <gitty58@gmail.com>
2023-02-01 09:36:46 +0900

    # 배포 설정 2

--- src/main/java/com/example/todo/HealthCheckController.java
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -2,11 +2,13 @@ package com.example.todo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class HealthCheckController { //서버 잘 돌아가는지 확인용도 localhost:8080
     @GetMapping("/")
     public ResponseEntity<?> check(){
--- src/main/java/com/example/todo/config/CorsConfig.java
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -10,11 +10,11 @@ public class CorsConfig implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         //api cors 정책 설정
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") //api요청 허용 URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600); //3600초 기다리셈
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("http://localhost:3000") //api요청 허용 URL
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(3600); //3600초 기다리셈
     }
 }
